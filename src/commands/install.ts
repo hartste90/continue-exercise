00de203f0994a0eb2023f03406633c777e416976
Steve Hart <hartste90@gmail.com>
2024-08-28 19:18:35 -0700

    recursively find dependencies from an object list

--- src/commands/install.ts
+++ src/commands/install.ts
@@ -2,6 +2,7 @@ import fs from "fs";
 import { Dependency } from "../types";
 import { installPackages } from "../util/download";
 import { nodeModulesPath, packageJsonPath } from "../util/paths";
+import { execSync } from "child_process";
 
 export async function installAllDependencies() {
   console.log("Installing dependencies...");
@@ -19,7 +20,38 @@ export async function installAllDependencies() {
 
   // -------------------------------------------------
   // TODO -> Determine the full list of dependencies to download
-  const dependenciesToDownload: Dependency[] = [];
+
+  function getAllDependencies(): Dependency[] {
+    const dependencies: Dependency[] = [];
+
+    //extracts dependencies from an object of dependencies with versions
+    function extractDependencies(deps: Dependency) {
+      if (!deps) return;
+      //iterate over all listed dependencies
+      for (const [name, version] of Object.entries(deps || {})) {
+        dependencies.push({ name, version });
+        //create a synchronous process to use npm to get its metadata
+        const npmView = execSync(`npm view ${name}@${version} --json`, {
+          encoding: "utf8",
+        });
+        //parse out the dependencies of that package
+        const parsedDependencies = JSON.parse(npmView).dependencies;
+        //recursively extract dependencies
+        extractDependencies(parsedDependencies);
+      }
+    }
+
+    //iterate over topLevelDependencies and extract subdependencies
+    extractDependencies(topLevelDependencies);
+
+    return dependencies;
+  }
+
+  const dependenciesToDownload = getAllDependencies();
+
+  // const dependenciesToDownload: Dependency[] = [
+  //   { name: "is-thirteen", version: "2.0.0" },
+  // ];
   // -------------------------------------------------
 
   await installPackages(dependenciesToDownload);
